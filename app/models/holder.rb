18304368132ee95879da0cbf928243b72d8e3fcf
Igor Ladkin <dgkplan@googlemail.com>
2015-07-06 14:56:47 +0300

    Create Holder model.

--- app/models/holder.rb
+++ app/models/holder.rb
@@ -3,20 +3,27 @@ class Holder < ActiveRecord::Base
 
   before_save :format_name, :format_city, :format_date
 
+  scope :lastname_starts_with, ->(letters) { where('name like ?', "% #{letters}%") }
+
+  def self.refresh_info_for(letters)
+    lastname_starts_with(letters).delete_all
+    Holder.create(PMI::Scraper.new.get_info_for(letters))
+  end
+
   private
 
   def format_name
     current_name_value = name
-    name_details = current_name_value.split(/\s+/).map(:strip)
+    name_details = current_name_value.split(/\W+/).map { |w| w.strip.capitalize }
 
-    return self.name = name_details.first.capitalize if name_details.size == 1
+    return self.name = name_details.first if name_details.size == 1
 
-    formatted_name = [name_details.first, name_details.last].join(' ').capitalize
+    formatted_name = [name_details.first, name_details.last].join(' ')
     self.name = formatted_name
   end
 
   def format_city
-    self.city = city.capitalize
+    self.city = city.split.map(&:capitalize).join(' ')
   end
 
   def format_date
--- spec/factories/holders.rb
+++ spec/factories/holders.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :holder do
-    name "MyString"
-    city "MyString"
-    country "MyString"
-    credential "MyString"
-    date_of_certification "2015-07-06"
-    status "MyString"
+    name 'John Doe'
+    city 'Moscow'
+    country 'Russian Federation'
+    credential 'PMP'
+    date_of_certification '2015-07-06'
+    status 'Active'
   end
 end
