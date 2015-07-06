4fb3fe728b5066c3a3d4cd45c64bcbc228396b69
Igor Ladkin <dgkplan@googlemail.com>
2015-07-06 16:45:43 +0300

    Edit checking format of search param with regexp.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,10 @@
 class PagesController < ApplicationController
+  before_action :check_format_for_search, only: :search
+
   def index
   end
 
   def search
-    unless params[:s].present?
-      flash[:warning] = 'Please enter lastname or couple letters lastname starts with.'
-      redirect_to root_path
-      return true
-    end
-
     force_update_flag = params.fetch(:force, 'false') == 'true'
 
     @holders = Holder.lastname_starts_with params[:s]
@@ -17,5 +13,25 @@ class PagesController < ApplicationController
     return render :search unless force_update
 
     @holders = Holder.refresh_info_for params[:s]
+
+    if @holders.empty?
+      flash[:alert] = 'There are no holders with that lastname.'
+      redirect_to root_path
+      return true
+    end
+
+    @holders
+  end
+
+  private
+
+  def check_format_for_search
+    unless params[:s] =~ /\A[a-z ,.'-]+\z/i
+      flash[:warning] = 'Please enter lastname or couple letters lastname starts with.'
+      redirect_to root_path
+      return true
+    end
+
+    true
   end
 end
